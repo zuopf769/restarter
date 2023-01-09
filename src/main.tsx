d49b45bd39efda1e50b66001c7fa415b6a0c9fc9
hyoban <hi@hyoban.cc>
2023-01-09 17:32:05 +0800

    chore: setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom'
+import routes from '~react-pages'
+
 import './index.css'
+
+const App = () => {
+  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
