73ffc3f237fdcc2da2098b1e17bea6e124911d12
hyoban <hi@hyoban.cc>
2023-01-10 17:59:39 +0800

    fix: add user not found info

--- src/network/useUser.ts
+++ src/network/useUser.ts
@@ -1,4 +1,4 @@
-import originalAxios from 'axios'
+import originalAxios, { AxiosError } from 'axios'
 import useSWR from 'swr'
 
 const baseUrl = 'https://jsonplaceholder.typicode.com/'
@@ -7,10 +7,18 @@ const axios = originalAxios.create({
   baseURL: baseUrl,
 })
 
-const fetcher = (url: string) => axios.get<User>(url).then((res) => res.data)
+const userFetcher = (id: string) =>
+  axios
+    .get<User>(id)
+    .then((res) => res.data)
+    .catch((err: AxiosError) => {
+      if (err.response?.status === 404) {
+        return null
+      }
+    })
 
 export default function useUser(id: number) {
-  const { data, error, isLoading } = useSWR(`/users/${id}`, fetcher)
+  const { data, error, isLoading } = useSWR(`/users/${id}`, userFetcher)
 
   return {
     user: data,
--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -7,7 +7,11 @@ function Userinfo() {
   const { user, isLoading, isError } = useUser(Number(id))
   if (isLoading) return <div>Loading...</div>
   if (isError) return <div>Error</div>
-  return <div>{user && <div>{user.name}</div>}</div>
+  return (
+    <div>
+      {user != null ? <div>{user.name}</div> : <div>User not found</div>}
+    </div>
+  )
 }
 
 export default Userinfo
