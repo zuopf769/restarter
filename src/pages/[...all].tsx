35c59cfa8aab085bb03d87e10103a64ede4332e9
hyoban <hi@hyoban.cc>
2023-01-10 18:01:48 +0800

    feat: add 404 page

---
+++ src/pages/[...all].tsx
@@ -0,0 +1,9 @@
+function NotFoundPage() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <h1 className="text-xl">oops, page not found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
