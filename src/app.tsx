f122488d5cce8b0d4a31379ab3f5d624ea1c2b8c
hyoban <hi@hyoban.cc>
2023-01-12 14:07:10 +0800

    fix: i18n for ant design

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,7 @@
 import { ConfigProvider, theme } from 'antd'
+import enUS from 'antd/locale/en_US'
+import zhCN from 'antd/locale/zh_CN'
+import { useTranslation } from 'react-i18next'
 import { RouterProvider } from 'react-router'
 import { createBrowserRouter } from 'react-router-dom'
 import routes from '~react-pages'
@@ -8,18 +11,25 @@ import { useDark } from '~/hooks/useDark'
 import '@unocss/reset/tailwind.css'
 import 'virtual:uno.css'
 import '~/locales/i18n'
-import './main.css'
+import '~/main.css'
+
+const i18nToLocale = {
+  en: enUS,
+  zh: zhCN,
+}
 
 const router = createBrowserRouter(routes)
 
 const App = () => {
   const [isDark] = useDark()
+  const [, i18n] = useTranslation()
 
   return (
     <ConfigProvider
       theme={{
         algorithm: isDark ? theme.darkAlgorithm : theme.defaultAlgorithm,
-      }}>
+      }}
+      locale={i18nToLocale[i18n.language as keyof typeof i18nToLocale] ?? enUS}>
       <RouterProvider router={router}></RouterProvider>
     </ConfigProvider>
   )
